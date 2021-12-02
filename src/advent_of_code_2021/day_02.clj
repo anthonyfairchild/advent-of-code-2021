3f0f54c50adb547983ca4be51b9b80877c3ef4dd
Anthony Fairchild <anthony.fairchild@tapad.com>
2021-12-02 08:58:34 -0500

    Day 2

---
+++ src/advent_of_code_2021/day_02.clj
@@ -0,0 +1,40 @@
+(ns advent-of-code-2021.day-02
+  (:require [clojure.java.io :refer [resource]]
+            [clojure.string :refer [split]]))
+
+(def input (-> "day_02_input.txt"
+               resource
+               slurp
+               (split #"\n")
+               (->> (map #(mapv read-string (split % #" "))))))
+
+(def test-input [['forward 5]
+                 ['down 5]
+                 ['forward 8]
+                 ['up 3]
+                 ['down 8]
+                 ['forward 2]])
+
+(defn problem-1
+  [input]
+  (let [[x y] (reduce (fn [[x y] [d n]]
+                        (case d
+                          forward [(+ x n) y]
+                          down    [x (+ y n)]
+                          up      [x (- y n)]))
+                      [0 0] input)]
+    (* x y)))
+
+(defn problem-2
+  [input]
+  (let [[x y] (reduce (fn [[x y a] [d n]]
+                        (case d
+                          forward [(+ x n) (+ y (* a n)) a]
+                          down    [x y (+ a n)]
+                          up      [x y (- a n)]))
+                      [0 0 0] input)]
+    (* x y)))
+
+#_input
+#_(problem-1 input)
+#_(problem-2 input)
